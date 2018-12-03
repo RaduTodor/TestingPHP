0195cddc6d85e9cf5340657a6cf1f0b29ad6a1fa
Cosmin Polifronie <cosmin.polifronie@gmail.com>
2018-12-03 17:06:32 +0200

    start composer on page load; add router class

--- public/index.php
+++ public/index.php
@@ -1,4 +1,6 @@
 <?php
+require __DIR__ . '/../vendor/autoload.php';
+
 session_start();
 if(!isset($_SESSION["username"]))
 {
---
+++ src/Router.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Framework;
+
+
+class Router
+{
+    public function route(array $static_routes, ?string $url, ?string $query) {
+        $params = array();
+        $this->getControllerName($static_routes, null, $controller, $action);
+        $split_query = null;
+
+        // remove the query string from the URL and make sure the link ends with a slash
+        if (isset($url)) {
+            $url = preg_replace('/\?.*/', '', $url);
+            $url = rtrim($url, '/') . '/';
+        }
+
+        // split the query string into an array
+        if (isset($query)) {
+            parse_str($query, $split_query);
+        }
+
+        // static route assignment
+        if (isset($static_routes[$url])) {
+            $this->getControllerName($static_routes, $url, $controller, $action);
+        }
+
+        $controller->{$action}($params, $split_query);
+    }
+
+    private function getControllerName(?array $routes, ?string $url, &$controller, &$action) {
+        if (!isset($routes[$url])) {
+            $url = null;
+        }
+
+        $controllerName = $routes[$url]['controller'];
+        $action = $routes[$url]['action'];
+
+        require_once '../app/controllers/' . $controllerName . '.php';
+
+        $controller = new $controllerName();
+    }
+}
