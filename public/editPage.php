d313cb2f29e17a3eeb200e01a54193fddf6fae63
Cosmin Polifronie <cosmin.polifronie@gmail.com>
2018-12-03 18:36:23 +0200

    add local bootstrap, jquery; fix routing path

--- public/editPage.php
+++ public/editPage.php
@@ -9,8 +9,8 @@
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<link href="../public/index.css" rel="stylesheet" id="bootstrap-css">
-<script src="../public/index.js"></script>
+<link href="css/index.css" rel="stylesheet" id="bootstrap-css">
+<script src="js/index.js"></script>
 <div class="container">
     	<div class="row">
 			<div class="col-md-6 col-md-offset-3">
