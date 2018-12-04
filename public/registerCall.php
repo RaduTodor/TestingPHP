a4b2b10a404eb01ae7d7cea0df5263d76262a745
Cosmin Polifronie <cosmin.polifronie@gmail.com>
2018-12-04 22:16:59 +0200

    remove files from old project

--- public/registerCall.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once "register.php";
-registerUserInDb($_POST["username"],$_POST["password"],$_POST["email"], $pdo);
-require_once "authenticateCall.php";
--- public/resetPasswordCall.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once "resetPassword";
-resetPassword($_POST["password"],$_POST["confirm_password"], $pdo);
-header("Location: editPage.php");
